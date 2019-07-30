f306cddb0c3e5d9417b37b4f49d4b8c3e37076f5
jsmith13 <theintangiblefatman@gmail.com>
2019-07-30 17:34:23 +0000

    Changed from write.table to fwrite.

--- compile_dataset.R
+++ compile_dataset.R
@@ -1,25 +1,24 @@
-## compile the two halves of the dataset into a single csv file
+## compile the smaller portions of the dataset into a single csv file
 # the compiled file is too large to fit into RAM, so we will write it line by line
 
-# load the first half
+# load the first portion
 load("MFCC_descriptors_part1")
 
-# write the first line w/ headers
-write.table(MFCC_descriptors[1, ], file = "MFCC_descriptors_compiled.csv", append = FALSE, quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
+# write the first portion with headers
+data.table::fwrite(MFCC_descriptors.1, file = "~/MFCC_descriptors_compiled.csv", append = FALSE)
 
-# append the remaining lines w/o headers
-for (i in 2:dim(MFCC_descriptors)[1]) {
-  write.table(MFCC_descriptors[i, ], file = "MFCC_descriptors_compiled.csv", append = TRUE, quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
-}
+# remove first portion from memory
+rm(MFCC_descriptors.1)
 
-# remove first from memory
-rm(MFCC_descriptors)
-
-# load the second half
-load("MFCC_descriptors_part2")
-
-# append the second half lines w/o headers
-for (i in 1:dim(MFCC_descriptors)[1]) {
-  write.table(MFCC_descriptors[i, ], file = "MFCC_descriptors_compiled.csv", append = TRUE, quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
+# write the remaining portions
+for (i in 2:4){
+  # load the current portion
+  load(paste("MFCC_descriptors_part", i, sep = ""))
+  
+  # append the contents to the csv file
+  data.table::fwrite(get(paste("MFCC_descriptors", i, sep = ".")), file = "~/MFCC_descriptors_compiled.csv", append = TRUE)
+  
+  # remove the R table from memory
+  rm(list = paste("MFCC_descriptors", i, sep = "."))
 }
 
